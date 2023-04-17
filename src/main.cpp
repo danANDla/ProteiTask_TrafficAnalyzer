629f0a3b83ba57fd9fe8b47344a1fe9b8409030b
archandla <d.a.n.202021@gmail.com>
2023-04-18 01:54:04 +0300

    main func
    capturing and parsing http and https packets

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,24 @@
 #include <iostream>
 
 #include <packetReader.h>
+#include <packetCapturer.h>
+#include <deviceManager.h>
+#include <packetParser.h>
 
-int main() {
-    readingTest();
+
+int main(int argc, char** argv) {
+    if(argc < 2) {
+        printf("expected device name as argument\n");
+        return 1;
+    } else if (argc > 3){
+        printf("expected 1 argument\n");
+        return 1;
+    }
+    std::string devName;
+    devName = argv[1];
+    std::string ip;
+    if(getDevByName(devName, ip)) return 1;
+    captureTest(ip);
+    parseAll();
     return 0;
 }
